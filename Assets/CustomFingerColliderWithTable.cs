db7710d0b6cb24eb3d16ddd8e580d2548ba72f52
Aaditya Vaze <aadityavikasvaze@gmail.com>
2024-04-23 16:35:24 -0400

    change to v64

--- Assets/CustomFingerColliderWithTable.cs
+++ Assets/CustomFingerColliderWithTable.cs
@@ -1,18 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-
 public class CustomFingerColliderWithTable : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private OVRSkeleton rightHandSkeleton;
+    private OVRBone indexTipBone, middleTipBone;
+    public float tableSurfaceHeight = 0.69f; // Set this to the Y position of your table surface
+
+
+    [SerializeField]
+    private TMP_Text debugTextViewIndex, debugTextViewMiddle;
     void Start()
     {
-        
+        // Automatically find the right hand OVRSkeleton
+        OVRSkeleton[] skeletons = FindObjectsOfType<OVRSkeleton>();
+        foreach (var skeleton in skeletons)
+        {
+            if (skeleton.GetSkeletonType() == OVRSkeleton.SkeletonType.HandRight)
+            {
+                rightHandSkeleton = skeleton;
+                break;
+            }
+        }
+
+        if (rightHandSkeleton == null)
+        {
+            debugTextViewIndex.text = "Right hand OVRSkeleton not found.";
+            Debug.LogError("Right hand OVRSkeleton not found.");
+            return;
+        }
+
+        // Find the index tip bone
+        foreach (var bone in rightHandSkeleton.Bones)
+        {
+            if (bone.Id == OVRSkeleton.BoneId.Hand_IndexTip)
+            {
+                indexTipBone = bone;
+                //break;
+            }
+             if (bone.Id == OVRSkeleton.BoneId.Hand_MiddleTip)
+            {
+                middleTipBone = bone;
+               // break;
+            }
+
+        }
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (indexTipBone != null)
+        {
+            // Check if the Y position of the index tip is less than or equal to the table surface height
+            if (indexTipBone.Transform.position.y <= tableSurfaceHeight)
+            {
+                Debug.Log("Index tip is below or at the table surface height!");
+                 debugTextViewIndex.text = "Collided index";
+              
+        }
+          
+                else{
+                       debugTextViewIndex.text = "Not collided";
+                }
     }
+
+     if (middleTipBone != null)
+        {
+            // Check if the Y position of the index tip is less than or equal to the table surface height
+            if (middleTipBone.Transform.position.y <= tableSurfaceHeight)
+            {
+                Debug.Log("Index tip is below or at the table surface height!");
+                 debugTextViewMiddle.text = "Collided middle";
+              
+        }
+          
+                else{
+                       debugTextViewMiddle.text = "Not collided";
+                }
+    }
+
+}
 }
